7690eba5a5b48ef3f19aef9431f353d4ba03df32
Yago <62577739+YagoCoutto@users.noreply.github.com>
2024-12-13 08:31:44 -0300

    rotas e header

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,27 @@
 import React from 'react';
 import './App.css';
-import Api from './api_test/Api';
+//import Api from './api_test/Api';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import Home from './components/Home';
+import Login from './components/login/Login';
+import Conta from './components/conta/Conta';
 
 const App = () => {
-  return <div>
-    <Api/>
-  </div>;
+  return (
+    <div>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/conta" element={<Conta />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
+    </div>
+  );
 };
 
 export default App;
