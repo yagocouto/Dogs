debe5dc24cc1a2d1ecf378f4142861c1eb99d6cd
yagocoutto <yagoig8@gmail.com>
2024-12-14 12:39:15 -0300

    validacao de formulario

--- src/components/login/LoginForm.jsx
+++ src/components/login/LoginForm.jsx
@@ -5,30 +5,27 @@ import Button from '../forms/Button';
 import useForm from '../../Hooks/useForm';
 
 const LoginForm = () => {
-  const username = useForm('email');
+  const username = useForm();
   const password = useForm();
-
-  console.log(username);
 
   function handleSubmit(event) {
     event.preventDefault();
 
-    //puxar o token do usuario para efetuar o login
-    fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify(),
-    })
-      .then((res) => {
-        console.log(res);
-        return res.json();
+    if (username.validate() && password.validate()) {
+      fetch('https://dogsapi.origamid.dev/json/jwt-auth/v1/token', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(),
       })
-      .then((json) => {
-        console.log(json);
-        return json;
-      });
+        .then((res) => {
+          return res.json();
+        })
+        .then((json) => {
+          return json;
+        });
+    }
   }
 
   return (
@@ -36,7 +33,7 @@ const LoginForm = () => {
       <h1>Login</h1>
       <form action="" onSubmit={handleSubmit}>
         <Input label="Usuario" type="text" name="username" {...username} />
-        <Input label="Senha" type="password" name="password" />
+        <Input label="Senha" type="password" name="password" {...password} />
         <Button>Entrar</Button>
       </form>
       <Link to="/login/criar">Cadastro</Link>
