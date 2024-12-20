811ffb674964b7906463d09cc7f6d8c68c66ea65
Yago <62577739+YagoCoutto@users.noreply.github.com>
2024-12-20 14:15:39 -0300

    Feed

--- src/components/Feed/FeedModal.jsx
+++ src/components/Feed/FeedModal.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const FeedModal = () => {
   return (
-    <div>FeedModal</div>
+    <div></div>
   )
 }
 
--- src/components/Feed/FeedPhotos.jsx
+++ src/components/Feed/FeedPhotos.jsx
@@ -1,23 +1,34 @@
 import React from 'react';
-import FeedPhotoItem from './FeedPhotoItem';
+import FeedPhotosItem from './FeedPhotosItem';
 import useFetch from '../../Hooks/useFetch';
+import { PHOTOS_GET } from '../../api';
+import Error from '../Helper/Error';
+import Loading from '../Helper/Loading';
+import styles from './FeedPhotos.module.css';
 
 const FeedPhotos = () => {
   const { data, loading, error, request } = useFetch();
 
   React.useEffect(() => {
     async function fetchPhotos(params) {
-      const {url, options} = PHOTOS_GET()
-      request(URL,options)
+      const { url, options } = PHOTOS_GET({ page: 1, total: 6, user: 0 });
+      const { response, json } = await request(url, options);
+      console.log(json);
     }
     fetchPhotos();
-  }, []);
+  }, [request]);
 
-  return (
-    <div>
-      <FeedPhotoItem />
-    </div>
-  );
+  if (error) return <Error error={error} />;
+  if (loading) return <Loading />;
+  if (data)
+    return (
+      <ul className={`${styles.feed} animeLeft`}>
+        {data.map((photo) => (
+          <FeedPhotosItem key={photo.id} photo={photo} />
+        ))}
+      </ul>
+    );
+  else return null;
 };
 
 export default FeedPhotos;
