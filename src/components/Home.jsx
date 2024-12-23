9137e23999995261ccb5471e9a2d6cf01bdd5897
Yago <62577739+YagoCoutto@users.noreply.github.com>
2024-12-23 10:02:47 -0300

    svg loading criado

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,7 +3,7 @@ import Feed from './Feed/Feed';
 
 const Home = () => {
   return (
-    <section className='container mainContainer'>
+    <section className="container mainContainer">
       <Feed />
     </section>
   );
